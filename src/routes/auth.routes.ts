c0588c2885d979ed0ed8812a8179c470670947ce
Omar Cruz Rendon <omarcr.96@gmail.com>
2026-03-14 01:41:10 -0600

    FIX: route name fixed to /auth/singup

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -10,7 +10,7 @@ import {
 const router = Router();
 
 router.post("/login", rateLimiter(15 * 60 * 1000, 10), login);
-router.post("/signup", signUpBusiness);
+router.post("/singup", signUpBusiness);
 router.post("/reset-password", rateLimiter(60 * 60 * 1000, 5), PasswordReset);
 router.post("/password-update", passwordUpdated);
 
